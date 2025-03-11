0f0272cfa0ff9baca265334715fc63d67b0dfffb
user <user@example.com>
2025-03-11 18:11:08 +0200

    day 2 daily challenge

---
+++ week1/day2/exercises_xp/daily_challenge.py
@@ -0,0 +1,19 @@
+from random import sample
+
+user_sentence = input("Please enter your sentence. It should be precisely 10 symbols: ")
+
+if len(user_sentence) < 10:
+    print("String is not long enough")
+elif len(user_sentence) > 10:
+    print("String is too long")
+else:
+    print(user_sentence[0])
+    print(user_sentence[-1])
+
+    print()
+    for i in range(len(user_sentence)):
+        print(user_sentence[:i+1])
+
+    print()
+    print(''.join(sample(user_sentence, len(user_sentence))))
+
