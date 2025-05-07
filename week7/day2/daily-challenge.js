acabc91a58768def4ffcc7671c45cbbf787cac70
user <user@example.com>
2025-05-07 14:50:53 +0300

    week 7 day 2 daily challenges

---
+++ week7/day2/daily-challenge.js
@@ -0,0 +1,22 @@
+// Not Bad
+let sentence = "The movie is not that bad, I like it"
+
+let wordNot = sentence.indexOf('not')
+let wordBad = sentence.indexOf('bad')
+
+if (wordNot < wordBad && wordBad >= 0 && wordNot >= 0) {
+    sentence = sentence.slice(0, wordNot) + 'good' + sentence.slice(wordBad + 3)
+}
+console.log(sentence)
+
+// Stars
+for (let i = 1; i <= 6; i++) {
+    console.log('* '.repeat(i))
+}
+for (let i = 1; i <= 6; i++) {
+    let result = ''
+    for (let j = 0; j < i; j++){
+        result += '* '
+    }
+    console.log(result)
+}
