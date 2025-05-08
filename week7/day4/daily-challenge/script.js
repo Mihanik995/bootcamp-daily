836725085b2d3c6b09afc28df55a521db075855d
user <user@example.com>
2025-05-08 18:38:51 +0300

    week 7 day 4 daily challenge

---
+++ week7/day4/daily-challenge/script.js
@@ -0,0 +1,17 @@
+let planets = {
+    Mercury: '#1a1a1a',
+    Venus: '#e6e6e6',
+    Earth: '#2f6a69',
+    Mars: '#993d00',
+    Jupiter: '#b07f35',
+    Saturn: '#b08f36',
+    Uranus: '#5580aa',
+    Neptune: '#366896'
+}
+for (let [planet, color] of Object.entries(planets)){
+    let newDiv = document.createElement('div')
+    newDiv.innerText = planet
+    newDiv.style.background = color
+    newDiv.className = 'planet'
+    document.getElementsByTagName('section')[0].appendChild(newDiv)
+}
